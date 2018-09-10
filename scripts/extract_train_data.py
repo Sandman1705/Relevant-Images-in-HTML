b5b7bc069fffb0101bd530e6395a18523a556407
Sandman1705 <sandman1705@gmail.com>
2018-09-10 19:23:02 +0200

    Added script for extracting train data

---
+++ scripts/extract_train_data.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.insert(0, '../src')
+import os
+dir_name = os.path.dirname(sys.argv[0])
+if(len(dir_name)!=0):
+	os.chdir(os.path.dirname(sys.argv[0]))
+
+import data_preprocess
+import pandas as pd
+
+def main():
+
+	data = data_preprocess.process_data_set("../data/train", "labels.json")
+	data = data_preprocess.change_bool_to_int(data)
+	data.to_csv("imgdata_train.csv", index=False)
+	print(data.groupby('is_relevant').count())
+
+#if __name__ == "__main__":
+#	main()
+
+main()
