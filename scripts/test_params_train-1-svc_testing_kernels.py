9b4732bd3ab4f178c98b5c546b58096968a5d455
Sandman1705 <sandman1705@gmail.com>
2018-09-10 19:26:02 +0200

    Added scripts for testing kernels for SVC

---
+++ scripts/test_params_train-1-svc_testing_kernels.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.insert(0, '../src')
+import os
+os.chdir(os.path.dirname(sys.argv[0]))
+
+from svc_models import kernel_test
+import pandas as pd
+
+def main():
+
+	data = pd.read_csv("../data/imgdata_train.csv", index_col='page')
+	results, best_params, (f1_score, acc_score) = kernel_test(data)
+	#print("Best parameters:", best_params)
+	#print("F1:", f1_score, "Accuracy:", acc_score)
+	results.to_csv("results_train_1-svc_testing_kernels.csv")
+
+#if __name__ == "__main__":
+#	main()
+
+main()
